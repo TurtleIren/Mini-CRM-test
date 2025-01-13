ee853a4f629166b1ecb7cb7d7c54cd338c5b6bd6
TurtleIren <i26031966@gmail.com>
2025-01-13 18:17:05 +0200

    add Company & Employee models, tables, factories, seeder

---
+++ Mini-CRM-test/database/factories/EmployeeFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EmployeeFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'company_id' => Company::factory(),
+            'email' => $this->faker->email,
+            'phone' => $this->faker->phoneNumber,
+        ];
+    }
+}
+
--- Mini-CRM-test/database/seeders/DatabaseSeeder.php
+++ Mini-CRM-test/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
+use App\Models\Employee;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -25,5 +27,9 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Company::factory(5)->create()->each(function ($company) {
+            Employee::factory(2)->create(['company_id' => $company->id]);
+        });
     }
 }
